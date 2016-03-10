61d9b26c51de9407e0e9b9492419b09783db27ac
chris owens <owens322@umn.edu>
2016-03-10 09:46:58 -0600

    basic update functionality working

--- updater.py
+++ updater.py
@@ -1,12 +1,4 @@
 #!/usr/bin/env python
-'''
-1. List all repos in a text file
-
-2. Iterate through all items in text file
-	download makepkg, dif with current on drive
-	if not different delete makepkg
-
-'''
 
 import sys
 import urllib.request
@@ -23,11 +15,16 @@ CLONE_PATH = "/home/cdo/AUR/"
 CONFIG_FILE = "list.txt"
 
 
-rePackageName = r'(?s)(?<=/pkgbase/atom-editor/">).+?(?=</a></td>)'
+rePackageName = r'(?s)(?<=<td class="wrap"><a href="/pkgbase/).+?(?=/">)'
 reUpdateTime = """(?s)(?<=<th>Last Updated: </th>
 			<td>).+?(?=</td>)"""
 
+
+##########################################
+#			Helper Functions			 #
+##########################################
 def getPackageName(body):
+	sss = rePackageName.format()
 	r = re.search(rePackageName, body)
 	return r.group()
 
@@ -43,52 +40,68 @@ def parseDateString(dateString):
 	dtObj = datetime.datetime.strptime(dateString, "%Y-%m-%d %H:%M")
 	return dtObj.timestamp()
 
-def clonePackage(package):
-	pkgPath = CLONE_PATH + package.name
-	if(os.path.isfile(pkgPath)):
-		call(["mv", pkgPath, pkgPath + ".bkp"])
-	call(["mkdir", pkgPath])
-	call(["git", "clone", package.gitPath, pkgPath])
-
 def getHttpResponseString():
 	resp = urllib.request.urlopen(site)
 	return resp.read().decode("utf-8")
 
+def clonePackage(package):
+	pkgPath = CLONE_PATH + package.name
+	bkpPath = CLONE_PATH + package.name + ".bkp"
 
-########################################
-#		Status Flags				   #
-########################################
+	if(os.path.isdir(bkpPath)):
+		call(["rm", "-rf", bkpPath])
+
+	if(os.path.isdir(pkgPath)):
+		print("creating backup")
+		call(["mv", pkgPath, bkpPath])
+
+	call(["mkdir", pkgPath])
+	call(["git", "clone", package.gitPath, pkgPath])
+
+def makeThePkg(package):
+	cwd = os.getcwd()
+	pkgPath = CLONE_PATH + package.name
+	os.chdir(pkgPath)
+	call(["makepkg", "-sri"])
+	os.chdir(cwd)
+
+##########################################
+#			  Status Flags				 #
+##########################################
 forceUpdate = False
 debug = False
+nothingToUpdate = True
 
 
+##########################################
+#			Argv Parasing				 #
+##########################################
 for v in sys.argv:
 	if v == "-f" or v == "-force":
 		forceUpdate = True
 	if v == "-d" or v == "-debug":
 		debug = True
 
+
+##########################################
+#				Main Loop				 #
+##########################################
 with open(CONFIG_FILE, "r") as f:
 	for site in f:
 		site.rstrip()
 		pack = Package()
 		respString = getHttpResponseString()
 
-		pack.name = packageName = getPackageName(respString)
+		pack.name = getPackageName(respString)
 		pack.updateTime = parseDateString(getUpdateTime(respString))
 		pack.gitPath = AUR_CLONE_URL + pack.name + ".git"
 		pack.localPath = CLONE_PATH + pack.name + "/PKGBUILD"
-		#packageName = getPackageName(respString)
-		#updateTime = parseDateString(getUpdateTime(respString))
-		#gitPath = AUR_CLONE_URL + packageName + ".git"
-
-		#packagePath = CLONE_PATH + packageName + "/PKGBUILD"
 
 		if os.path.isfile(pack.localPath):
 			pack.installTime = os.path.getmtime(pack.localPath)
 			timeDif = pack.updateTime - pack.installTime
 		else:
-			clonePackage(packageName)
+			clonePackage(pack)
 			exit()
 
 		if debug == True:
@@ -97,7 +110,28 @@ with open(CONFIG_FILE, "r") as f:
 			print("Git Path: {}".format(pack.gitPath))
 			print("{} last modified local: {}".format(pack.name, pack.installTime))
 			print("file time dif = {}".format(timeDif))
+		#else:
+		#	os.system('clear')
 
 
-		if timeDif < 0:
-			clonePackage(pack)
+		if (timeDif > 0) or (forceUpdate == True):
+			update = False
+			nothingToUpdate = False
+			while 1:
+				print("Update {} (y/n)?".format(pack.name))
+				userResp = input("")
+				if (userResp == "y") or (userResp == "Y") or (userResp == "yes") or (userResp == "Yes"):
+					update = True
+					break
+				elif (userResp == "n") or (userResp == "N") or (userResp == "no") or (userResp == "No"):
+					break
+				else:
+					print("Invalid Response")
+			if update == True:
+				clonePackage(pack)
+				makeThePkg(pack)
+		else:
+			print("{} is up to date".format(pack.name))
+
+	if nothingToUpdate == True:
+		print("Everything is up to date!")
